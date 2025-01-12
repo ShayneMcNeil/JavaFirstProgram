f6e5431a3318ef29f8e8aba40f51ca6dda2be9f2
ShayneMcNeil <w0454809@nscc.ca>
2025-01-11 23:43:14 -0400

    First Java Program

---
+++ Main.java
@@ -0,0 +1,14 @@
+/*
+
+    Assignment: Setting Up Java and Writing Your First Program
+    Student Name: Shayne McNeil
+    Date: January 11, 2025
+    Student ID: W0454809
+
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
